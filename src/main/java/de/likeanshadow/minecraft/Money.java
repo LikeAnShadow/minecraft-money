38635c41604964b0d47060f120bda48453e6d15d
tobi04kl <tobi04kl@gmail.com>
2024-03-21 13:42:59 +0100

    Yeet

--- src/main/java/de/likeanshadow/minecraft/Money.java
+++ src/main/java/de/likeanshadow/minecraft/Money.java
@@ -1,13 +1,18 @@
 package de.likeanshadow.minecraft;
 
+import jdk.jfr.internal.LogLevel;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class Money extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getLogger().log(Level.INFO, "Starting Money-Plugin");
     }
 
     @Override
